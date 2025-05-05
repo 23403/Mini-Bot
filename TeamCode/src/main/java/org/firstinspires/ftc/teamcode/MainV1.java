52b21c2cae0867f1677c0163edad5f97f36139a0
23403 Laptop1 <codeconductors23403@gmail.com>
2025-05-05 16:43:47 -0400

    slides code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainV1.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
+
+import xyz.nin1275.constants.PIDFKCoefficients;
+import xyz.nin1275.controllers.PID;
+import xyz.nin1275.subsystems.SlidesSS;
 
 @TeleOp
 
@@ -21,13 +27,27 @@ public class MainV1 extends LinearOpMode {
     private CRServo clawL;
     private CRServo clawR;
     private IMU imu;
-
+    public static PIDFKCoefficients eaPID = new PIDFKCoefficients(
+            0.02,
+            0,
+            0,
+            0.08
+    );
+    public static PIDFKCoefficients taPID = new PIDFKCoefficients(
+            0.03,
+            0,
+            0,
+            0.18
+    );
+    public static double CPR = 537.7; // counts per revolution
+    public static double INCHES_PER_REV = 16; // how far the arm travels linearly per motor revolution
 
     // todo: write your code here
     @Override
     public void runOpMode() {
 
-        DcMotor arm = hardwareMap.get(DcMotor.class, "arm");
+        DcMotorEx extendArm = hardwareMap.get(DcMotorEx.class, "Extend");
+        DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "arm");
         DcMotor chassisBL = hardwareMap.get(DcMotor.class, "chassisBL");
         DcMotor chassisBR = hardwareMap.get(DcMotor.class, "chassisBR");
         DcMotor chassisFL = hardwareMap.get(DcMotor.class, "chassisFL");
@@ -37,11 +57,17 @@ public class MainV1 extends LinearOpMode {
         chassisBL.setDirection(DcMotor.Direction.REVERSE);
         chassisFL.setDirection(DcMotor.Direction.REVERSE);
         clawL.setDirection(CRServo.Direction.REVERSE);
+        PID eaController = new PID(Math.sqrt(eaPID.P), eaPID.I, eaPID.D);
+        PID taController = new PID(Math.sqrt(taPID.P), taPID.I, taPID.D);
+        SlidesSS sliders = new SlidesSS(extendArm, eaController, 0, eaPID.F, CPR, INCHES_PER_REV, true);
+        SlidesSS armSS = new SlidesSS(arm, taController, 0, taPID.F, CPR, INCHES_PER_REV, true);
+
         // init
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
                 // loop
+                // drive
                 double forward = -gamepad1.left_stick_y;//forward
                 double strafe = gamepad1.left_stick_x;//strafe
                 double turn = gamepad1.right_stick_x;//rotation
@@ -55,13 +81,8 @@ public class MainV1 extends LinearOpMode {
                 chassisBL.setPower(chassisBLPower);
                 chassisFR.setPower(chassisFRPower);
                 chassisBR.setPower(chassisBRPower);
-                if (gamepad1.dpad_up) {
-                    arm.setPower(0.7);
-                } else if (gamepad1.dpad_down) {
-                    arm.setPower(-0.5);
-                } else {
-                    arm.setPower(0);
-                }
+                sliders.update(gamepad1.dpad_up, gamepad1.dpad_down);
+                armSS.update(gamepad1.right_stick_y > 0.3, gamepad1.right_stick_y < 0.3);
                 if (gamepad1.right_bumper) {
                     clawL.setPower(1);
                     clawR.setPower(1);
